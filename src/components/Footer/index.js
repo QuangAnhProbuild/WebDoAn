f50710fcb62bf1347980aba9bb5c99a203d751e7
Quang Anh <qu.anh261299@gmail.com>
2022-01-15 10:10:48 +0700

    âcgdfsaf

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = ({ ...otherProps }) => {
   return (
     <footer className="bg-gray-600">
-      <div className="w-full max-w-7xl mx-auto py-14 px-2.5">
+      <div className="w-full max-w-7xl mx-auto py-14 px-2.5 text-center">
         <span className="text-base text-black">
-          COPYRIGHT © 2021 - Đinh Mạnh Hiếu
+          COPYRIGHT © 2021 - Trần Quang Anh
         </span>
       </div>
     </footer>
